77e2f7aed6eeea038c551770996f76b0955e0562
snowpooll <snowpoollovely@gmail.com>
2016-08-14 16:04:00 +0000

    heroku app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if logged_in?
-    if logged_in_user
+    if logged_in?
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page])
     end
   end
